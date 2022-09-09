96f519465e5e99dccef45aae455eb575dbb12405
Anton Illenberger <i@gidroonn.ru>
2022-09-09 18:54:35 +0400

    fixed bug with checking length

--- Task1.py
+++ Task1.py
@@ -21,17 +21,21 @@ def main():
     prompt = input("1 - Encode or 2 - Decode?: ")
     string = input("Enter the string: ")
     key = input("Enter the key: ")
-
-    if len(key) < len(string):
-        print("Error: key must be longer or equal to string.")
-        quit()
     
     if prompt == '1':
+        if len(key) < len(string):
+            print("Error: key must be longer or equal to string.")
+            quit()
         print(f"Encoded string: {encode(string, key)}")
+
     elif prompt == '2':
+        if len(key) < len(string.split()):
+            print("Error: key must be longer or equal to string.")
+            quit()
         print(f"Decoded string: {decode(string, key)}")
     else:
         print("Enter please 1 or 2.")
+
 
 if __name__ == '__main__':
     main()
