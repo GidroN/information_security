fecf553d53b7c8a9b7cc0c15535b4f5d4170083a
Anton Ill <gidrontv1@gmail.com>
2024-01-19 22:03:48 +0400

    :feature: solved task 2 var 2

--- 4 course/Var 2/Task2.py
+++ 4 course/Var 2/Task2.py
@@ -1,13 +1,18 @@
+from typing import Tuple, Any, List
+
 from prettytable import PrettyTable
 import os
 import sqlite3
 
 
 def category_exist_handler(func):
-    def wrapper(self, category: str, *args, **kwargs):
-        db = DataBase()
-        if not db.check_table_exists(category):
-            return f'Категории {category} не существует. Сначала добавьте ее.'
+    def wrapper(self, category: str | list, *args, **kwargs):
+        categories = [category] if isinstance(category, str) else category
+        missing_categories = [cat for cat in categories if not self.db.check_table_exists(cat)]
+
+        if missing_categories:
+            missing_str = ', '.join(missing_categories)
+            return f'Категории {missing_str} не существует. Сначала добавьте ее/их.'
 
         return func(self, category, *args, **kwargs)
 
@@ -32,7 +37,12 @@ class DataBase:
             open(path, 'w').close()
         return sqlite3.connect(path)
 
-    def get_all_rows_from_category(self, category: str) -> list[tuple[str, int]]:
+    def get_money_rows_from_category(self, category: str, operation: str) -> list[tuple[int]]:
+        prompt = f"SELECT SUM(change) FROM {category.capitalize()} WHERE operation='{operation}'"
+        self.cur.execute(prompt)
+        return self.cur.fetchone()[0]
+
+    def get_info_rows_from_category(self, category: str) -> list[tuple[str, int]]:
         prompt = f"SELECT description, operation, change FROM {category.capitalize()};"
         self.cur.execute(prompt)
         return self.cur.fetchall()
@@ -54,7 +64,7 @@ class DataBase:
                     ); 
 
                     INSERT INTO {category} (balance, operation, change, description) 
-                    VALUES ({balance}, 'Пополнение', {balance}, 'Создание новой категории');
+                    VALUES ({balance}, '{self.TOP_UP}', {balance}, 'Создание новой категории');
                     """
 
         self.cur.executescript(prompt)
@@ -120,10 +130,10 @@ class Wallet:
         return True
 
     @category_exist_handler
-    def print_category_stats(self, category: str):
+    def print_category_stats(self, category: str) -> None:
         print(f'Статистика для категории {category.capitalize()}: ')
         table = PrettyTable(['Описание операции', 'Тип операции', 'Сумма'])
-        items = self.db.get_all_rows_from_category(category)
+        items = self.db.get_info_rows_from_category(category)
 
         for row in items:
             table.add_row(list(row))
@@ -133,17 +143,47 @@ class Wallet:
 
         print(table)
 
+    def _calculate_percent_spend_for_each_category(self, categories: list[str]) -> list[tuple[str, float]]:
+        result = []
+        spend_money = []
+        earned_money = []
+
+        for category in categories:
+            total_spend_in_category = self.db.get_money_rows_from_category(category, self.db.TOP_DOWN)
+            total_earned_in_category = self.db.get_money_rows_from_category(category, self.db.TOP_UP)
+
+            spend_money.append((category, total_spend_in_category))
+            earned_money.append((category, total_earned_in_category))
+
+        total_earned = sum(item[1] for item in earned_money)
+
+        for category, amount_spend in spend_money:
+            amount_spend = 0.0 if not amount_spend else amount_spend
+            percent = round((amount_spend / total_earned) * 100, 2)
+            result.append((category, percent))
+
+        return result
+
+    @category_exist_handler
+    def calculate_percent_spend_for_each_category(self, categories: list) -> None:
+        spend_in_categories = self._calculate_percent_spend_for_each_category(categories)
+        table = PrettyTable(['Категория', '% от потраченной суммы'])
+        for category, percent in spend_in_categories:
+            table.add_row([category, percent])
+
+        print(table)
+
 
 if __name__ == '__main__':
     db = DataBase()
-    db.get_all_rows_from_category('Food')
     wallet = Wallet()
-    wallet.print_category_stats('food')
+    # wallet.print_category_stats('food')
     # wallet.add_category('Food')
     # wallet.add_category('Clothes')
 #     wallet.top_up('Food', 100,)
-    wallet.top_up('Clothes', 100)
+#     wallet.top_up('Clothes', 100)
     # wallet.top_down('Food', 50)
-    wallet.send_to_category('Food', 'Clothes', 30)
-
-
+    # wallet.send_to_category('Food', 'Clothes', 30)
+    # print(db.get_money_rows_from_category('food', DataBase().TOP_UP))
+    # print(wallet._calculate_percent_spend_for_each_category(['food', 'clothes']))
+    wallet.calculate_percent_spend_for_each_category(['food', 'clothes'])
