17d8a947ac2306b0ea0e12e5e9eb5d5ca7e76f5b
Anton ill <gidrontv1@gmail.com>
2024-01-23 20:49:40 +0400

    :feature: solved task 2 var 3. NOT MY SOLUTION!

--- 4 course/Var3/Task2.py
+++ 4 course/Var3/Task2.py
@@ -1,28 +1,37 @@
 import random
-import math
 
 
 class Box:
     def __init__(self, **balls):
-        self.balls = []
-        self.total_balls = len(self.balls)
-        for color, count in balls.items():
-            self.balls.extend([color] * count)
+        self.balls = balls
 
-    def calculate_probability(self, balls: dict) -> tuple[float, float]:
-        color_comb = 1
-        total_balls_chosen = 0
+    def draw_balls(self, attempts):
+        drawn_balls = []
+        for _ in range(attempts):
+            ball_color = random.choice(list(self.balls.keys()))
+            drawn_balls.append(ball_color)
+        return drawn_balls
 
-        for color, count in balls.items():
-            color_comb *= math.comb(self.balls.count(color), count)
-            total_balls_chosen += count
 
-        total_comb = math.comb(len(self.balls), total_balls_chosen)
-        result = color_comb / total_comb
+def counter(box, expects, nums, num_exp):
+    success_count = 0
 
-        return round(result * 100, 3), round(result, 3)
+    for _ in range(num_exp):
+        drawn_balls = box.draw_balls(nums)
+        ball_count = {color: drawn_balls.count(color) for color in set(drawn_balls)}
+
+        if all(ball_count.get(color, 0) >= count for color, count in expects.items()):
+            success_count += 1
+
+    probability = success_count / num_exp
+    return probability
 
 
 if __name__ == '__main__':
-    box = Box(black=5, white=5, red=5, blue=5)
-    print(box.calculate_probability({'black': 1, 'blue': 1, 'red': 1, 'white': 1}))
+    box1 = Box(черный=5, синий=3, зеленый=8)
+    expected_result = {'черный': 2, 'зеленый': 3}
+    num_attempts = 8
+    num_experiments = 1000
+
+    probability = counter(box=box1, expects=expected_result, nums=num_attempts, num_exp=num_experiments)
+    print(f"Вероятность вытащить {expected_result} за {num_attempts} попыток: {probability}")
